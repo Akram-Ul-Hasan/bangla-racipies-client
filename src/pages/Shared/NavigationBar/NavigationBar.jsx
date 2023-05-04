22076f81fff359e67f397d1724951b564f280376
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-05-04 14:11:00 +0600

    observe onAuthStateChange and log out added

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../providers/AuthProvider';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavigationBar = () => {
-
-  const {user} =useContext(AuthContext);
-
-    return (
-        <div>
-            <div className="navbar bg-primary-focus">
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then()
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  return (
+    <div>
+      <div className="navbar bg-primary-focus">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-2xl">Bangla Recipes</a>
         </div>
@@ -18,23 +23,33 @@ const NavigationBar = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-            <Link to="/blog">Blog</Link>
+              <Link to="/blog">Blog</Link>
             </li>
-            <li>
-            <Link to="">{user.displayName}</Link>
-            </li>
-            <li>
-            <Link to="/login/login">Login</Link>
-            </li>
-            <li>
-            <Link to="/login/register">Register</Link>
-            </li>
+            <li></li>
+            {user && (
+              <li>
+                <Link to="">{user?.displayName}</Link>
+              </li>
+            )}
+            {user ? (
+              <li>
+                <p onClick={handleLogOut}>Logout</p>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/login/register">Register</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
-            
-        </div>
-    );
+    </div>
+  );
 };
 
 export default NavigationBar;
