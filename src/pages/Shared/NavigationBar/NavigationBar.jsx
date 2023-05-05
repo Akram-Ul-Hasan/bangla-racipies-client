f4a52fb794e974574d2cf3d17d1cf6cbce1c8fcd
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-05-05 21:57:28 +0600

    make home page responsive

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -14,11 +14,60 @@ const NavigationBar = () => {
   };
   return (
     <div>
-      <div className="navbar bg-primary-focus">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-2xl">Bangla Recipes</a>
+      <div className="navbar bg-primary">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <ActiveLink to="/">Home</ActiveLink>
+              </li>
+              <li>
+                <ActiveLink to="/blog">Blog</ActiveLink>
+              </li>
+              <li></li>
+              {user && (
+                <li>
+                  <Link to="">{user?.displayName}</Link>
+                </li>
+              )}
+              {user ? (
+                <li>
+                  <p onClick={handleLogOut}>Logout</p>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <ActiveLink to="/login/login">Login</ActiveLink>
+                  </li>
+                  <li>
+                    <ActiveLink to="/login/register">Register</ActiveLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-3xl">Bangla Recipes</a>
         </div>
-        <div className="flex-none">
+        <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
               <ActiveLink to="/">Home</ActiveLink>
@@ -48,7 +97,9 @@ const NavigationBar = () => {
             )}
           </ul>
         </div>
+        
       </div>
+      
     </div>
   );
 };
